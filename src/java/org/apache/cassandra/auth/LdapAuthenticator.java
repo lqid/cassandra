07e7f7f7a75f6804b2d5017cacac81277d94e74b
klevi <lqid@users.noreply.github.com>
2016-07-29 17:40:03 +1000

    First successful connection to LDAP server using Apache API, and inclusion of required API files.

--- src/java/org/apache/cassandra/auth/LdapAuthenticator.java
+++ src/java/org/apache/cassandra/auth/LdapAuthenticator.java
@@ -17,13 +17,19 @@
  */
 package org.apache.cassandra.auth;
 
-import org.apache.cassandra.exceptions.AuthenticationException;
-import org.apache.cassandra.exceptions.ConfigurationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.exceptions.*;
+
+import org.apache.directory.ldap.client.api.LdapConnection;
+import org.apache.directory.ldap.client.api.LdapNetworkConnection;
+
 
 /**
  * LdapAuthenticator is an IAuthenticator implementation
@@ -32,30 +38,61 @@ import java.util.Set;
  */
 public class LdapAuthenticator implements IAuthenticator{
 
-    private static final Logger logger = LoggerFactory.getLogger(PasswordAuthenticator.class);
+    private static final Logger logger = LoggerFactory.getLogger(LdapAuthenticator.class);
 
-    // Do not allow anonymous access.
-    public boolean requireAuthentication() {return true;}
-
-    @Override
-    public Set<? extends IResource> protectedResources() {
-        return null;
+    public void setupLdapConnection () throws Exception {
+        LdapConnection connection = new LdapNetworkConnection("192.168.1.200", 389 );
+        connection.setTimeOut(30);
+        connection.bind("uid=cassandra,ou=people,dc=klevi,dc=local", "cassandra");
+        connection.unBind();
+        connection.close();
     }
 
-    @Override
+    // Do not allow anonymous access.
+    public boolean requireAuthentication() { return true; }
+
+    public Set<DataResource> protectedResources()
+    {
+        // Also protected by CassandraRoleManager, but the duplication doesn't hurt and is more explicit
+        return ImmutableSet.of(DataResource.table(AuthKeyspace.NAME, AuthKeyspace.ROLES));
+    }
+
     public void validateConfiguration() throws ConfigurationException {
 
     }
 
-    @Override
     public void setup() {
-
+        try
+        {
+            setupLdapConnection();
+        }
+        catch (Exception e)
+        {
+            logger.trace("Error performing internal authentication", e);
+            throw new AuthenticationException(e.toString());
+        }
     }
 
-    @Override
     public SaslNegotiator newSaslNegotiator() {
-        return null;
+        return new PlainTextSaslAuthenticator();
     }
+
+    private class PlainTextSaslAuthenticator implements SaslNegotiator {
+
+        public byte[] evaluateResponse(byte[] clientResponse) throws AuthenticationException
+        {
+            return null;
+        }
+
+        public boolean isComplete() { return true; }
+
+        public AuthenticatedUser getAuthenticatedUser() throws AuthenticationException
+        {
+            return null;
+        }
+
+    }
+
 
     @Override
     public AuthenticatedUser legacyAuthenticate(Map<String, String> credentials) throws AuthenticationException {
