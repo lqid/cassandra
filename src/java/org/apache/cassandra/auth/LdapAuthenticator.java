431607a4f3c7537592ee0a1e4b183428780672e4
klevi <lqid@users.noreply.github.com>
2016-08-01 05:49:54 +1000

    Working through internal authentication process.

--- src/java/org/apache/cassandra/auth/LdapAuthenticator.java
+++ src/java/org/apache/cassandra/auth/LdapAuthenticator.java
@@ -18,6 +18,8 @@
 package org.apache.cassandra.auth;
 
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
@@ -27,6 +29,8 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.exceptions.*;
 
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 
@@ -39,17 +43,46 @@ import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 public class LdapAuthenticator implements IAuthenticator{
 
     private static final Logger logger = LoggerFactory.getLogger(LdapAuthenticator.class);
+    private static final byte NUL = 0;
 
-    public void setupLdapConnection () throws Exception {
-        LdapConnection connection = new LdapNetworkConnection("192.168.1.200", 389 );
-        connection.setTimeOut(30);
-        connection.bind("uid=cassandra,ou=people,dc=klevi,dc=local", "cassandra");
-        connection.unBind();
-        connection.close();
+    public LdapConnection ldapConnection = new LdapNetworkConnection("192.168.1.200", 389 );
+
+    private void setupLdapConnection () throws Exception {
+        ldapConnection.setTimeOut(30);
+        ldapConnection.bind("uid=cassandra,ou=people,dc=klevi,dc=local","cassandra");
+        //connection.unBind();
+        //connection.close();
     }
 
     // Do not allow anonymous access.
     public boolean requireAuthentication() { return true; }
+
+    private AuthenticatedUser authenticate(String username) throws AuthenticationException
+    {
+        System.out.println("Made it to authenticate!");
+        System.out.println("LDAP is connected? " + ldapConnection.isConnected());
+        System.out.println("LDAP is authenticated? " + ldapConnection.isAuthenticated());
+
+        try
+        {
+            ldapConnection.bind("uid=cassandra,ou=people,dc=klevi,dc=local","cassandra");
+            Entry authenticatingLdapUser = ldapConnection.lookup("uid="+username+",ou=people,dc=klevi,dc=local");
+            if (authenticatingLdapUser != null)
+            {
+                System.out.println(authenticatingLdapUser);
+                allowAuthentication(username);
+            }
+            else {
+                System.out.println("User is not found.");
+            }
+        }
+        catch (LdapException e){
+            System.out.println(e);
+            logger.trace("Error performing LDAP authentication", e);
+            throw new AuthenticationException(e.toString());
+        }
+        return new AuthenticatedUser("cassandra");
+    }
 
     public Set<DataResource> protectedResources()
     {
@@ -68,7 +101,7 @@ public class LdapAuthenticator implements IAuthenticator{
         }
         catch (Exception e)
         {
-            logger.trace("Error performing internal authentication", e);
+            logger.trace("Error setting up LDAP connection.", e);
             throw new AuthenticationException(e.toString());
         }
     }
@@ -77,18 +110,75 @@ public class LdapAuthenticator implements IAuthenticator{
         return new PlainTextSaslAuthenticator();
     }
 
+    // Allows authentication of user.
+    private AuthenticatedUser allowAuthentication(String username)
+    throws RequestExecutionException, AuthenticationException
+    {
+        System.out.println("User found: " + username);
+        return new AuthenticatedUser(AuthenticatedUser.SYSTEM_USERNAME);
+    }
+
     private class PlainTextSaslAuthenticator implements SaslNegotiator {
+
+        private boolean complete = false;
+        private String username;
+        private String password;
 
         public byte[] evaluateResponse(byte[] clientResponse) throws AuthenticationException
         {
+            decodeCredentials(clientResponse);
+            complete = true;
             return null;
         }
 
-        public boolean isComplete() { return true; }
+        public boolean isComplete() { return complete; }
 
         public AuthenticatedUser getAuthenticatedUser() throws AuthenticationException
         {
-            return null;
+            if (!complete)
+                throw new AuthenticationException("SASL negotiation not complete");
+            return authenticate(username);
+        }
+
+        /**
+         * SASL PLAIN mechanism specifies that credentials are encoded in a
+         * sequence of UTF-8 bytes, delimited by 0 (US-ASCII NUL).
+         * The form is : {code}authzId<NUL>authnId<NUL>password<NUL>{code}
+         * authzId is optional, and in fact we don't care about it here as we'll
+         * set the authzId to match the authnId (that is, there is no concept of
+         * a user being authorized to act on behalf of another with this IAuthenticator).
+         *
+         * @param bytes encoded credentials string sent by the client
+         * @return map containing the username/password pairs in the form an IAuthenticator
+         * would expect
+         * @throws javax.security.sasl.SaslException
+         */
+        private void decodeCredentials(byte[] bytes) throws AuthenticationException
+        {
+            logger.trace("Decoding credentials from client token");
+            byte[] user = null;
+            byte[] pass = null;
+            int end = bytes.length;
+            for (int i = bytes.length - 1 ; i >= 0; i--)
+            {
+                if (bytes[i] == NUL)
+                {
+                    if (pass == null)
+                        pass = Arrays.copyOfRange(bytes, i + 1, end);
+                    else if (user == null)
+                        user = Arrays.copyOfRange(bytes, i + 1, end);
+                    end = i;
+                }
+            }
+
+            if (user == null)
+                throw new AuthenticationException("Authentication ID must not be null");
+            if (pass == null)
+                throw new AuthenticationException("Password must not be null");
+
+            username = new String(user, StandardCharsets.UTF_8);
+            password = new String(pass, StandardCharsets.UTF_8);
+            System.out.println(username + password);
         }
 
     }
